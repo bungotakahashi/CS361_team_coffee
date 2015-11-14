386d28c026a281333effb85da67e20c11dcc0d32
bungotakahashi <takahasb@onid.orst.edu>
2015-11-13 23:06:53 -0800

    email setting added to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,17 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'https://serene-citadel-2648.herokuapp.com/users' } 
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'coffeecs361@gmail.com',
+      password:             'coffee1234',
+      authentication:       'plain',
+      enable_starttls_auto: true
+         }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
