4836c06360229a53d05f387e833be9bbf2a644eb
bungotakahashi <takahasb@onid.orst.edu>
2015-11-14 00:20:41 -0800

    hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'https://serene-citadel-2648.herokuapp.com' } 
+  config.action_mailer.default_url_options = { :host => "https://serene-citadel-2648.herokuapp"} 
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
