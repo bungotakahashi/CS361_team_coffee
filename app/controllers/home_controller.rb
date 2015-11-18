0841baec68d1b79ec8b02c7e58b97acd4a706a36
Trugger <supersleuth@mac.com>
2015-11-18 23:34:30 +0000

    subscription payment type feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,26 @@
 class HomeController < ApplicationController
-  
+  before_filter :authenticate_user!, except: [:index]
   def index
+  	if user_signed_in?
+  		redirect_to :action => "show"
+  	end
   end
 
   def show
+  end
+
+  def subscription
+  	@user=User.find(params[:user_id])
+  end
+
+  def subscription_create
+  	@x=payment_params
+  	@user = User.find(payment_params[:id])
+  	@user.update_attributes(payment_params) 
+  	redirect_to :action => "show"	
+  end
+
+  def payment_params
+		params.require(:user).permit(:id, :paymentMethod)
   end
 end
